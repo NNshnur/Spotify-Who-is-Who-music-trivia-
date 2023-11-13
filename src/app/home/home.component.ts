de32db618670f60991885a6a9713eb768b199a02
Mika Nelson <mika.nelson@protonmail.com>
2023-11-13 15:55:15 -0500

    feat: add configuration saving to homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import fetchFromSpotify, { request } from "../../services/api";
+import { Router } from "@angular/router";
 
 const AUTH_ENDPOINT =
   "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
@@ -11,16 +12,19 @@ const TOKEN_KEY = "whos-who-access-token";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   genres: String[] = ["House", "Alternative", "J-Rock", "R&B"];
-  selectedGenre: String = "";
+  selectedGenre: string = "";
+  numberOfSongs: number = 1;
+  numberOfArtists: number = 2;
   authLoading: boolean = false;
   configLoading: boolean = false;
   token: String = "";
 
   ngOnInit(): void {
     this.authLoading = true;
+    this.retrieveSettingsFromLocalStorage();
     const storedTokenString = localStorage.getItem(TOKEN_KEY);
     if (storedTokenString) {
       const storedToken = JSON.parse(storedTokenString);
@@ -58,7 +62,40 @@ export class HomeComponent implements OnInit {
 
   setGenre(selectedGenre: any) {
     this.selectedGenre = selectedGenre;
-    console.log(this.selectedGenre);
-    console.log(TOKEN_KEY);
+  }
+
+  setNumberOfArtists(numberOfArtists: any) {
+    this.numberOfArtists = numberOfArtists;
+  }
+
+  setNumberOfSongs(numberOfSongs: any) {
+    this.numberOfSongs = numberOfSongs;
+  }
+
+  submitAndStartGame() {
+    this.submitSettings();
+    this.startGame();
+  }
+
+  submitSettings() {
+    localStorage.setItem("numberOfArtists", this.numberOfArtists.toString());
+    localStorage.setItem("numberOfSongs", this.numberOfSongs.toString());
+    localStorage.setItem("selectedGenre", this.selectedGenre.toString());
+  }
+
+  startGame() {
+    // TODO: replace this with an actual url
+    this.router.navigateByUrl("");
+  }
+
+  retrieveSettingsFromLocalStorage() {
+    let savedArtists = localStorage.getItem("numberOfArtists");
+    let savedSongs = localStorage.getItem("numberOfSongs");
+    let savedGenre = localStorage.getItem("selectedGenre");
+    this.numberOfArtists = savedArtists
+      ? parseInt(savedArtists)
+      : this.numberOfArtists;
+    this.numberOfSongs = savedSongs ? parseInt(savedSongs) : this.numberOfSongs;
+    this.selectedGenre = savedGenre ? savedGenre : this.selectedGenre;
   }
 }
